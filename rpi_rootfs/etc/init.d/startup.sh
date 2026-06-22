e5b13e1f29f8b2f9d50de2f75e5f5c318fd72072
Vasu-Eng <kesharwanivasudev@gmail.com>
2026-06-23 01:26:16 +0530

    Add static IP fallback for DHCP failure

--- rpi_rootfs/etc/init.d/startup.sh
+++ rpi_rootfs/etc/init.d/startup.sh
@@ -6,7 +6,13 @@ echo "Bringing up Ethernet..."
 ifconfig eth0 up
 
 echo "Requesting DHCP lease..."
-udhcpc -i eth0
+
+if ! udhcpc -i eth0 -n -t 5
+then 
+   echo "DHCP failed!"
+   ifconfig eth0 192.168.1.200 netmask 255.255.255.0
+   echo "used static ip 192.168.1.200/24..."
+fi
 
 echo "Network configuration:"
 ifconfig eth0
