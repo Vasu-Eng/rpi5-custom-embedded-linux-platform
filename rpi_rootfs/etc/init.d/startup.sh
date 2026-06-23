4b2e5feb3bcead3d2c2e4e98e67913b754b43202
Vasu-Eng <kesharwanivasudev@gmail.com>
2026-06-23 12:35:57 +0530

    debug(dropbear): root login fails due to invalid shell

--- rpi_rootfs/etc/init.d/startup.sh
+++ rpi_rootfs/etc/init.d/startup.sh
@@ -6,16 +6,19 @@ echo "Bringing up Ethernet..."
 ifconfig eth0 up
 
 echo "Requesting DHCP lease..."
-
 if ! udhcpc -i eth0 -n -t 5
-then 
-   echo "DHCP failed!"
-   ifconfig eth0 192.168.1.200 netmask 255.255.255.0
-   echo "used static ip 192.168.1.200/24..."
+then
+    echo "DHCP failed, assigning fallback IP..."
+    ifconfig eth0 192.168.1.200 netmask 255.255.255.0
+     echo "used static ip 192.168.1.200/24..."
+
 fi
+
+echo "Starting Dropbear SSH..."
+mkdir -p /var/run
+dropbear  -E
 
 echo "Network configuration:"
 ifconfig eth0
-
 
 exit 0
